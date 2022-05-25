07bac8b2f666b2eb38785bf6a0cf8780806ec2f1
Manhnv <nguyenvietmanhit@gmail.com>
2022-05-25 19:28:29 +0700

    update

--- Day11_PHP_MySQL_CRUD/Codes_demo/crud/create.php
+++ Day11_PHP_MySQL_CRUD/Codes_demo/crud/create.php
@@ -1,24 +1,50 @@
 <!--create.php-->
-
-<h2>Form thêm mới sản phẩm</h2>
-<!--products:
-id, category_id, name, price, description, created_at
-Thông thường set name của input trùng với tên trường
--->
+<!--Bảng categories: id, name, description, created_at-->
+<?php
+require_once 'connection.php';
+// XỬ LÝ FORM:
+// B1: Debug:
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+// B2: Tạo biến chứa lỗi và kết quả:
+$error = '';
+$result = '';
+// B3: Ktra nếu submit form thì mới xử lý form:
+if (isset($_POST['submit'])) {
+    // B4: Lấy giá trị từ form
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    // B5: Validate form: phải nhập tất cả trường
+    if (empty($name) || empty($description)) {
+        $error = 'Phải nhập tất cả trường';
+    }
+    // B6: Xử lý logic chính chỉ khi ko có lỗi xảy ra:
+    if (empty($error)) {
+        //+ Thêm dữ liệu từ form vào bảng categories, cần kết
+        //nối CSDL
+        // + Nhúng file connection.php vào để sử dụng đc luôn
+        // biến kết nối, vị trí nhúng: đầu file
+        // -  Viết truy vấn:
+        $sql_insert = "INSERT INTO categories(name, description)
+        VALUES('$name', '$description')";
+        // - Thực thi truy vấn: INSERT trả về boolean
+        $is_insert = mysqli_query($connection, $sql_insert);
+        var_dump($is_insert);
+    }
+}
+// B7: Hiển thị error và result ra form
+?>
+<h3 style="color: red"><?php echo $error; ?></h3>
+<h3 style="color: green"><?php echo $result; ?></h3>
+<h2>Form thêm mới danh mục</h2>
 <form action="" method="post">
-    Nhập tên sp:
+    Nhập tên:
     <input type="text" name="name" value="" />
     <br />
-    Nhập giá sp:
-    <input type="text" name="price" value="" />
-    <br />
-    Chi tiết sp:
+    Chi tiết:
     <textarea name="description"></textarea>
     <br />
-<!--  Tạo 1 input ẩn để set giá trị mặc định cho category_id,
-  để xử lý đơn giản-->
-    <input type="hidden" name="category_id" value="1" />
-    <input type="submit" name="submit" value="Lưu sp" />
+    <input type="submit" name="submit" value="Lưu" />
     <a href="index.php">Về trang danh sách</a>
 </form>
-<!--Để code 1 chức năng: code giao diện trước -> code PHP-->
